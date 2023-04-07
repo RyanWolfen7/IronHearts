8dad4e6bbb1a8a92b20d2809860d9e087e7f4461
RyanWolfen7 <wolfen7lada@gmail.com>
2023-04-07 01:07:36 -0500

    added content to about

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,4 +1,5 @@
-const { Container, Grid } = require("@mui/material")
+import { FormattedMessage } from "react-intl"
+import { Container, Grid, Typography } from "@mui/material"
 
 const About = ({
 
@@ -7,7 +8,30 @@ const About = ({
     return (
         <Container fixed>
             <Grid container textAlign="center" placeItems="center">
-                
+                <Typography variant="h2">
+                    <FormattedMessage id="ourStory"/>
+                </Typography>
+                <Typography variant="body1">
+                    <FormattedMessage id="aboutIntro"/>
+                </Typography>
+                <Typography variant="body1">
+                    <FormattedMessage id="aboutIntro2"/>
+                </Typography>
+                <Typography variant="body1">
+                    <FormattedMessage id="about2"/>
+                </Typography>
+                <Typography variant="body1">
+                    <FormattedMessage id="about3"/>
+                </Typography>
+                <Typography variant="body1">
+                    <FormattedMessage id="about4"/>
+                </Typography>
+                <Typography variant="body1">
+                    <FormattedMessage id="about5"/>
+                </Typography>
+                <Typography variant="body1">
+                    <FormattedMessage id="about6"/>
+                </Typography>
             </Grid>
             
         </Container>
