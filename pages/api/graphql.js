1a66ee824252c9635bf2777466d212716980ebad
RyanWolfen7 <wolfen7lada@gmail.com>
2023-04-22 02:00:02 -0500

    initial apollo mongo setup, need to resolve db updates

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,22 +1,16 @@
 import { ApolloServer } from '@apollo/server';
 import { startServerAndCreateNextHandler } from '@as-integrations/next';
-import { gql } from 'graphql-tag';
+import MongoClientPromise from '@/lib/api/config/mongoDB'
+import typeDefs from '@/lib/api/typeDefs';
+import resolvers from '@/lib/api/resolvers';
 
-const resolvers = {
-    Query: {
-        hello: () => 'world',
-    },
-};
-
-const typeDefs = gql`
-    type Query {
-        hello: String
-    }
-`;
+MongoClientPromise()
 
 const server = new ApolloServer({
     resolvers,
-    typeDefs,
+    typeDefs   
 });
 
-export default startServerAndCreateNextHandler(server);
+export default startServerAndCreateNextHandler(server, {
+    context: async() => ({ db: await MongoClientPromise()})
+});
